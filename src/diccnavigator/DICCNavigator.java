79d1fac3a103337e4b56907faca44ee35bcba3d9
Saad Ahmed Pathan <153294722+saadpathan@users.noreply.github.com>
2024-05-03 12:30:01 +0800

    uploading src

---
+++ src/diccnavigator/DICCNavigator.java
@@ -0,0 +1,14 @@
+package diccnavigator;
+
+/**
+ *
+ * @author Hp
+ */
+public class DICCNavigator {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to DICC Navigator!");
+    }
+
+}
