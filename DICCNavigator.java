4d28fe19f8cd386fa7e41d6ade5bff0cd8094115
Saad Ahmed Pathan <153294722+saadpathan@users.noreply.github.com>
2024-05-03 12:21:41 +0800

    testing commit and push

--- DICCNavigator.java
+++ DICCNavigator.java
@@ -8,6 +8,10 @@ public class DICCNavigator {
     public static void main(String[] args) {
 
         System.out.println("Welcome to DICC Navigator!");
+        System.out.println("Welcome to DICC Navigator!");
+        System.out.println("Welcome to DICC Navigator!");
+        System.out.println("Welcome to DICC Navigator!");
+        System.out.println("Welcome to DICC Navigator!");
     }
 
 }
