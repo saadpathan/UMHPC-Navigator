b33e9eb9cea9cdca182b50486b1cda01ebb7edba
Saad Ahmed Pathan <153294722+saadpathan@users.noreply.github.com>
2024-05-03 11:58:17 +0800

    uploading the first java file

---
+++ DICCNavigator.java
@@ -0,0 +1,13 @@
+package dicc.navigator;
+
+public class DICCNavigator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to DICC Navigator!");
+    }
+
+}
